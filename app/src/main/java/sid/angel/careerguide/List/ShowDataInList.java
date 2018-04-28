bedbdc5981dddb17b577fc303035aeb196b17a66
Nikita Kothari <NikitaNikita.Kothari@lntinfotech.com>
2018-04-29 00:15:31 +0530

    list added by development branch

---
+++ app/src/main/java/sid/angel/careerguide/List/ShowDataInList.java
@@ -0,0 +1,14 @@
+package sid.angel.careerguide.List;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import sid.angel.careerguide.R;
+
+public class ShowDataInList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data_in_list);
+    }
+}
